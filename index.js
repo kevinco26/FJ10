6ffeaec1b5a768d0e5235319dd545cb29212895b
Gerry <gerardo.pineda94@gmail.com>
2018-09-03 20:07:53 -0500

    Cambiando file

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var http = require('http');
 var server = http.createServer(function(request, response) {
 
     response.writeHead(200, {"Content-Type": "text/plain"});
-    response.end("Hello Football judge! De parte de Gerardo y kevin. ");
+    response.end("Hello Football judge! De parte de Gerardo y kevin. Page is on maintenance ");
 
 });
 
